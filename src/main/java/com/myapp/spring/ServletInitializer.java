1b461392827e20f8382086cb0378ecdd1827ce4a
rahulvamsi <p.rahulvamsi@gmail.com>
2021-08-13 12:00:39 +0530

    updated pom2

--- src/main/java/com/myapp/spring/ServletInitializer.java
+++ src/main/java/com/myapp/spring/ServletInitializer.java
@@ -1,5 +1,12 @@
 package com.myapp.spring;
 
-public class ServletInitializer {
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProductAppApplication.class);
+    }
 }
+
+
