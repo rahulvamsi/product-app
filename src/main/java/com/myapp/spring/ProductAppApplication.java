0e8cef708ff35d4422d0ba7a5dd86aaf53a50652
rahulvamsi <p.rahulvamsi@gmail.com>
2021-08-20 14:52:46 +0530

    modified files

--- src/main/java/com/myapp/spring/ProductAppApplication.java
+++ src/main/java/com/myapp/spring/ProductAppApplication.java
@@ -3,8 +3,10 @@ package com.myapp.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+@ComponentScan({"com.mypackage.controller"})
 public class ProductAppApplication {
 
 	public static void main(String[] args) {
